f7082d51ad0b25fb8533b9d8200db5c7e0f0b76b
heily-tech <edelbteen@gmail.com>
2021-10-19 09:15:11 +0900

    signupAct_FIN

--- src/game/swimming/activities/SignUpActivity.java
+++ src/game/swimming/activities/SignUpActivity.java
@@ -1,52 +1,71 @@
 package game.swimming.activities;
 
+import game.swimming.MainActivity;
+
+import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
 
 //뒤로가기 버튼
 
 public class SignUpActivity extends JFrame{
-    JButton doBtn;
+    JButton doBtn, backBtn;
     JLabel idLabel, pwLabel;
     JTextField idField;
     JPasswordField pwField;
 
     public SignUpActivity() {
-        //JPanel btnPanel = new JPanel();
-        setLayout(null);
+        Color c = new Color(120, 209, 255);
+        JPanel btnPanel = new JPanel();
+        btnPanel.setBackground(c);
+        btnPanel.setLayout(null);
 
         idLabel = new JLabel("아이디 : ");
-        idLabel.setBounds(100, 200, 100, 35);
+        idLabel.setBounds(70, 180, 100, 45);
         //idLabel.getHorizontalTextPosition();
-        add(idLabel);
+        btnPanel.add(idLabel);
 
         idField = new JTextField();
-        idField.setBounds(200, 200, 150, 35);
-        add(idField);
+        idField.setBounds(185, 185, 150, 30);
+        btnPanel.add(idField);
 
         pwLabel = new JLabel("패스워드 : ");
-        pwLabel.setBounds(100, 240, 100, 35);
-        add(pwLabel);
+        pwLabel.setBounds(70, 220, 100, 45);
+        btnPanel.add(pwLabel);
 
         pwField = new JPasswordField();
-        pwField.setBounds(200, 240, 150, 35);
-        add(pwField);
+        pwField.setBounds(185, 225, 150, 30);
+        btnPanel.add(pwField);
 
         doBtn = new JButton("가입");
-        doBtn.setBounds(150, 300, 100, 35);
+        doBtn.setBounds(145, 320, 100, 35);
+        doBtn.setBackground(Color.WHITE);
         doBtn.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 dispose();
             }
         });
-        add(doBtn);
+        btnPanel.add(doBtn);
 
-        setTitle("My Swimming Game : Sign UP");
-        setSize(400, 500);
-        setResizable(false);
-        setLocationRelativeTo(null); //화면 중앙에 창 위치
-        setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        setVisible(true);
+        backBtn = new JButton();
+        backBtn.setBounds(20, 20, 50, 50);
+        backBtn.setContentAreaFilled(false);
+        ImageIcon backImg = new ImageIcon(MainActivity.class.getResource("res/btns/backBtn.png"));
+        backBtn.setIcon(backImg);
+        backBtn.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+        btnPanel.add(backBtn);
+
+        this.add(btnPanel);
+        this.setTitle("My Swimming Game : Sign UP");
+        this.setSize(400, 500);
+        this.setResizable(false);
+        this.setLocationRelativeTo(null); //화면 중앙에 창 위치
+        this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        this.setVisible(true);
     }
 }
 
