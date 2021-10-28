d815e16ad24d8bb26e3a16f20e98585e0a12582d
heily-tech <edelbteen@gmail.com>
2021-10-28 21:00:46 +0900

    10282100

--- src/game/swimming/activities/PlayActivity.java
+++ src/game/swimming/activities/PlayActivity.java
@@ -4,8 +4,10 @@ package game.swimming.activities;
 import game.swimming.MainActivity;
 
 import javax.swing.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-public class PlayActivity extends JPanel {
+public class PlayActivity extends JPanel implements KeyListener {
     private MainActivity main;
     private SelectModeActivity selectModeActivity;
 
@@ -15,9 +17,28 @@ public class PlayActivity extends JPanel {
         setOpaque(false);
         setLayout(null);
         setVisible(true);
+        addKeyListener(this);
+        setFocusable(true);
+    }
 
-        /*
-         템스피드 | 거리 | 네트워킹 | 영법
-         */
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_UP)
+            System.out.println("위로");
+        else if (e.getKeyCode() == KeyEvent.VK_DOWN)
+            System.out.println("아래로");
+        else if (e.getKeyCode() == KeyEvent.VK_LEFT)
+            System.out.println("왼쪽");
+        else if (e.getKeyCode() == KeyEvent.VK_RIGHT)
+            System.out.println("오른쪽");
     }
 }
